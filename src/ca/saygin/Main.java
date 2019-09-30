472e541a0b94b58aa1a6f204d9be10d1ac30e975
sayginguven <sayginguven@yahoo.com>
2019-09-30 12:30:28 -0700

    bigdecimal added

--- src/ca/saygin/Main.java
+++ src/ca/saygin/Main.java
@@ -1,38 +1,73 @@
 package ca.saygin;
 
+import java.math.BigDecimal;
+
 public class Main {
 
+    // global static variables are automaticly initialized with value
+    // 0
+    static int intVal;
     public static void main(String[] args) {
 
-        //different variables
-        //byte short int long
+//        System.out.println("global intVal value is " + intVal);
+//
+//        //different variables
+//        //byte short int long
+//
+//        System.out.println("Hello Java!");
+//        //primitive types
+//        byte byteNumber = Byte.MAX_VALUE;
+//        short shortNumber = Short.MIN_VALUE;
+//        int intNumber = 2_147_483_647 + 1;
+//        int negativeInt = -2_147_483_648 -1;
+//        System.out.println("overflow intNumber is : " + intNumber);// overflow
+//        System.out.println("underflow negativeInt is : " + negativeInt);// overflow
+//
+//        System.out.println("byte number value is " + byteNumber);
+//        System.out.println("short number value is " + shortNumber);
+//
+//        //casting
+//        byte aByteValue = 60;
+//        byte anotherByteValue = (byte)(aByteValue/2);
+//
+//        System.out.println("anotherByteValue 60/2 is : " + anotherByteValue);
+//
+//        int a = 130;
+//        //truncated
+//        byte b = (byte)a;
+//        System.out.println("truncated byte value is : " + b);//?
+//
+//        byte x = 127;
+//        //all good
+//        int y = x;
+//        System.out.println("converted byte value is : " + y);
+//
+//        // for long types use L at the end of value
+//        long longValue = 9_223_372_036_854_775_807L;//max long number
+//        System.out.println(longValue);
+//
+//        double cofeePrice = 2.75d;// d is optional but better to use it
+//        float teaPrice = 2.99f; // f is a must
+//
+//        int coffee = (int)cofeePrice;
+//        System.out.println("the int coffee price : " + coffee); // 2
+//        System.out.println("the rounded tea price is : " + Math.round(teaPrice)); // 3
+//        System.out.println("the rounded tea price is : " + Math.floor(teaPrice)); // 2.0
 
-        System.out.println("Hello Java!");
-        //primitive types
-        byte byteNumber = Byte.MAX_VALUE;
-        short shortNumber = Short.MIN_VALUE;
-        int intNumber = 2_147_483_647 + 1;
-        int negativeInt = -2_147_483_648 -1;
-        System.out.println(intNumber);// overflow
-        System.out.println(negativeInt);// overflow
+        System.out.println("(double)25 / 2 = " + (double)25 / 2);
+        System.out.println("25d / 2 = " + 25d / 2);
+        System.out.println("25f / 2 = " + 25f / 2);
+        System.out.println("(int)(25f / 2) = " + (int)(25f / 2));
 
-        System.out.println("byte number value is " + byteNumber);
-        System.out.println("short number value is " + shortNumber);
+        int test = (int)Math.pow(2,32);
+        System.out.println(test);
 
-        //casting
-        byte aByteValue = 60;
-        byte anotherByteValue = (byte)(aByteValue/2);
+        double dVal = .012;
+        double sum = dVal + dVal + dVal;
+        System.out.println("wrong calculation .12 + .12 + 12 = " + sum);
 
-        System.out.println(anotherByteValue);
-
-        int a = 130;
-        //truncated
-        byte b = (byte)a;
-        System.out.println(b);//?
-
-        byte x = 127;
-        //all good
-        int y = x;
-        System.out.println(y);
+        BigDecimal bigValue = new BigDecimal(Double.toString(dVal));
+        BigDecimal total  = bigValue.add(bigValue).add(bigValue);
+        System.out.println("exact result of .12 + .12 + 12 = " + total);
     }
 }
