63520119f7325f8dbe4e9788246a427fdfebc225
sayginguven <sayginguven@yahoo.com>
2019-09-30 10:23:12 -0700

    initial stage

--- src/ca/saygin/Main.java
+++ src/ca/saygin/Main.java
@@ -3,6 +3,6 @@ package ca.saygin;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello java");
+        System.out.println("hello java!");
     }
 }
