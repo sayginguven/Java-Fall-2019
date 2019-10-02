994ec65b55571b3917f717b52500addd310e8bbc
sayginguven <sayginguven@yahoo.com>
2019-10-02 11:02:50 -0700

    organized the code into different classes

---
+++ src/ca/saygin/Conditions.java
@@ -0,0 +1,123 @@
+package ca.saygin;
+
+public class Conditions {
+
+    static void conditions(){
+        int year = 2019;
+        //year = year + 1;
+        //pre increment
+        System.out.println("++year = " + ++year);// ? 2020
+        //post increment
+        System.out.println("year++ = " + year++);// ? 2020
+        //here the year value is 2021
+        System.out.println("year = " +year); // ? 2021
+
+        year %= 2;
+        System.out.println("year %= 2 = " + year); // ? 1
+
+
+        //conditions
+        //if blocks
+        int age = 18;
+        System.out.println("age is " + age);
+        if (age >= 120) {
+            System.out.println("sorry you are dead :(");
+        } else if (age >= 18) {
+            System.out.println("you are adult");
+        } else {
+            System.out.println("you are NOT adult");
+        }
+
+        boolean islightson = true;
+        System.out.println("lights are on ? " + islightson);
+        if (islightson) {
+            System.out.println("can you turn off the light?");
+        } else {
+            System.out.println("can you turn on the light?");
+        }
+
+        int studentMark = 70;
+        double gpa;
+        //ternary operator
+        gpa = studentMark >= 80 ? 4.0 : 3.0;
+        System.out.println("student gpa is " + gpa);
+
+        // 1 - green 2 - red
+        int light = 1;
+        boolean go;
+        go = light != 2 ? true : false;
+        System.out.println("Should I pass if the light is green? : " + go);
+        System.out.println("Should I pass if the light is red? : " + !go);
+
+        // && AND
+        // || OR
+
+        age = 17;
+        boolean parentPermission = false;
+
+        if ( age >= 18 || parentPermission  ){
+            System.out.println("you are allowed to do stuff");
+        } else {
+            System.out.println("you need to grow up or you need permission");
+        }
+
+        if (age < 18 && parentPermission){
+            System.out.println("you need to grow up or you need permission");
+        } else {
+            System.out.println("you are allowed to do stuff");
+        }
+
+        int dayOfWeek = 3; // 0 .... 6
+        // 0 - sunday
+        // 1 - monday
+        // ... so on
+        if(dayOfWeek == 0){
+            System.out.println("its sunday");
+        } else if (dayOfWeek == 1){
+            System.out.println("its monday");
+        } else if (dayOfWeek == 2){
+            System.out.println("its tuesday");
+        } else if (dayOfWeek == 3){
+            System.out.println("its wednesday");
+        } else if (dayOfWeek == 4){
+            System.out.println("its thursday");
+        } else if (dayOfWeek == 5){
+            System.out.println("its friday");
+        } else if (dayOfWeek == 6){
+            System.out.println("its saturday");
+        } else {
+            System.out.println("ops there is no such a day");
+        }
+
+        switch (dayOfWeek) {
+            case 0:
+                System.out.println("its sunday");
+                break;
+            case 1:
+                System.out.println("its monday");
+                break;
+            case 2:
+                System.out.println("its tuesday");
+                break;
+            case 3:
+                System.out.println("its wednesday");
+                break;
+            case 4:
+                System.out.println("its thursday");
+                break;
+            case 5:
+                System.out.println("its friday");
+                break;
+            case 6:
+                System.out.println("its saturday");
+                break;
+            default:
+                System.out.println("ops there is no such a day");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/ca/saygin/Main.java
+++ src/ca/saygin/Main.java
@@ -1,73 +1,11 @@
 package ca.saygin;
 
-import java.math.BigDecimal;
-
 public class Main {
 
-    // global static variables are automaticly initialized with value
-    // 0
-    static int intVal;
     public static void main(String[] args) {
 
-        System.out.println("global intVal value is " + intVal);
+        Variables.variables();
+        Conditions.conditions();
 
-        //different variables
-        //byte short int long
-
-        System.out.println("Hello Java!");
-        //primitive types
-        byte byteNumber = Byte.MAX_VALUE;
-        short shortNumber = Short.MIN_VALUE;
-        int intNumber = 2_147_483_647 + 1;
-        int negativeInt = -2_147_483_648 -1;
-        System.out.println("overflow intNumber is : " + intNumber);// overflow
-        System.out.println("underflow negativeInt is : " + negativeInt);// overflow
-
-        System.out.println("byte number value is " + byteNumber);
-        System.out.println("short number value is " + shortNumber);
-
-        //casting
-        byte aByteValue = 60;
-        byte anotherByteValue = (byte)(aByteValue/2);
-
-        System.out.println("anotherByteValue 60/2 is : " + anotherByteValue);
-
-        int a = 130;
-        //truncated
-        byte b = (byte)a;
-        System.out.println("truncated byte value is : " + b);//?
-
-        byte x = 127;
-        //all good
-        int y = x;
-        System.out.println("converted byte value is : " + y);
-
-        // for long types use L at the end of value
-        long longValue = 9_223_372_036_854_775_807L;//max long number
-        System.out.println(longValue);
-
-        double cofeePrice = 2.75d;// d is optional but better to use it
-        float teaPrice = 2.99f; // f is a must
-
-        int coffee = (int)cofeePrice;
-        System.out.println("the int coffee price : " + coffee); // 2
-        System.out.println("the rounded tea price is : " + Math.round(teaPrice)); // 3
-        System.out.println("the rounded tea price is : " + Math.floor(teaPrice)); // 2.0
-
-        System.out.println("(double)25 / 2 = " + (double)25 / 2);
-        System.out.println("25d / 2 = " + 25d / 2);
-        System.out.println("25f / 2 = " + 25f / 2);
-        System.out.println("(int)(25f / 2) = " + (int)(25f / 2));
-
-        int test = (int)Math.pow(2,32);
-        System.out.println(test);
-
-        double dVal = .012;
-        double sum = dVal + dVal + dVal;
-        System.out.println("wrong calculation .12 + .12 + 12 = " + sum);
-
-        BigDecimal bigValue = new BigDecimal(Double.toString(dVal));
-        BigDecimal total  = bigValue.add(bigValue).add(bigValue);
-        System.out.println("exact result of .12 + .12 + 12 = " + total);
     }
 }
