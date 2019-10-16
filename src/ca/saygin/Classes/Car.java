f20fa59691e6784e2e783864a25615816c02af45
sayginguven <sayginguven@yahoo.com>
2019-10-16 12:33:23 -0700

    added speed field, accelerate method

--- src/ca/saygin/Classes/Car.java
+++ src/ca/saygin/Classes/Car.java
@@ -8,21 +8,36 @@ public class Car {
     private String color;
     private boolean automatic;
     private double fuel;
+    private double speed;
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
 
     public Car(){
-        this("unknown", "unknown", "black", true, 10.0);
+        this("unknown", "unknown", "black", true, 10.0,0);
     }
 
     public Car(String make, String model) {
-        this(make, model, "white", false, 25.0d);
+        this(make, model, "white", false, 25.0d, 0d);
     }
 
-    public Car(String make, String model, String color, boolean automatic, double fuel) {
+    public Car(String make, String model, String color, boolean automatic, double fuel, double speed) {
         this.make = make.toLowerCase();
         this.model = model.toLowerCase();
         this.color = color;
         this.automatic = automatic;
         this.fuel = fuel;
+        if(speed >=0 && speed <=180) {
+            this.speed = speed;
+        }else{
+            this.speed = 0;
+        }
+
     }
 
     public String getMake() {
@@ -63,6 +78,15 @@ public class Car {
 
     public void setFuel(double fuel) {
         this.fuel = fuel;
+    }
+
+    public void accelerate(){
+
+        if(speed <= 170){
+            speed += 10;
+        }
+        System.out.println("your speed is : " + speed);
+
     }
 
     @Override
