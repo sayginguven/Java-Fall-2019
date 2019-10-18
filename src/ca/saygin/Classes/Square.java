31c44099412c86c53e47c40d83710b9c2fc1069f
sayginguven <sayginguven@yahoo.com>
2019-10-18 11:29:15 -0700

    square class updated

--- src/ca/saygin/Classes/Square.java
+++ src/ca/saygin/Classes/Square.java
@@ -1,4 +1,36 @@
 package ca.saygin.Classes;
 
-public class Square {
+public class Square extends Shape {
+
+    private double length;
+
+    public Square(){
+
+    }
+    public Square(double length) {
+        this.length = length;
+    }
+
+    public Square(String name, double length) {
+        super(name);
+        this.length = length;
+    }
+
+    @Override
+    public double area() {
+        return length*length;
+    }
+
+    @Override
+    public double perimeter() {
+        return 4*length;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
 }
