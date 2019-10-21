aab16f40f7ebe5971f6489db59329b6e61643523
sayginguven <sayginguven@yahoo.com>
2019-10-21 10:17:07 -0700

    instanceof example

--- src/ca/saygin/Main.java
+++ src/ca/saygin/Main.java
@@ -1,66 +1,31 @@
 package ca.saygin;
 
-import ca.saygin.Classes.*;
-import ca.saygin.Classes.Box;
+import ca.saygin.Classes.Cat;
+import ca.saygin.Classes.Dog;
+import ca.saygin.Classes.Pet;
 
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<Integer> arrayList = new ArrayList<>();// flexible size
-        List<Integer> linkedList = new LinkedList<>();// flexible size
+        Pet[] pets = new Pet[2];
 
-        System.out.println("this is array list");
-        arrayList.add(101);
-        arrayList.add(102);
-        arrayList.add(100);
-        arrayList.add(104);
-        arrayList.add(105);
+        pets[0] = new Cat();
+        pets[1] = new Dog();
 
-        arrayList.set(2,103);
 
-        for(Integer i : arrayList) {
-            System.out.println(i);
+        for(Object i : pets){
+
+            if (i instanceof Cat) {
+                ((Cat)i).move();
+            }
+            if (i instanceof Dog) {
+                ((Dog)i).move();
+            }
+
         }
-
-        System.out.println("arrayList.get(0) : " + arrayList.get(0));
-        System.out.println(arrayList.contains(5));//false
-        System.out.println(arrayList.contains(105));//true
-
-        System.out.println("this is linked list");
-        linkedList.add(101);
-        linkedList.add(102);
-        linkedList.add(100);
-        linkedList.add(104);
-        linkedList.add(105);
-
-        linkedList.set(2,103);
-
-        for(Integer i : linkedList) {
-            System.out.println(i);
-        }
-
-        System.out.println("arrayList.get(0) : " + linkedList.get(0));
-        System.out.println(linkedList.contains(5));//false
-        System.out.println(linkedList.contains(105));//true
-
-
-        LinkedList<Shape> shapesList = new LinkedList<>();
-        shapesList.add(new Box());
-        shapesList.add(new Square());
-        shapesList.add(new Circle());
-        shapesList.add(new Rectangle());
-
-        for(Shape s : shapesList){
-            System.out.println(s.getName());
-        }
-
 
 
     }
