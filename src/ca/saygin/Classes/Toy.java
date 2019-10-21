e947ebb136c4e0900c9f4be696c38f56455a8aae
sayginguven <sayginguven@yahoo.com>
2019-10-21 11:30:02 -0700

    added toy class for composition

---
+++ src/ca/saygin/Classes/Toy.java
@@ -0,0 +1,33 @@
+package ca.saygin.Classes;
+
+public class Toy {
+
+    private String name;
+    private String color;
+
+    public Toy(){
+        name = "a toy";
+        color = "white";
+    }
+
+    public Toy(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
