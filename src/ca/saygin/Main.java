13181a923cc2d33d33b8437e5b09c083da50f4e7
sayginguven <sayginguven@yahoo.com>
2019-10-21 11:31:27 -0700

    added tests for dog-toy composition

--- src/ca/saygin/Main.java
+++ src/ca/saygin/Main.java
@@ -3,6 +3,7 @@ package ca.saygin;
 import ca.saygin.Classes.Cat;
 import ca.saygin.Classes.Dog;
 import ca.saygin.Classes.Pet;
+import ca.saygin.Classes.Toy;
 
 import java.util.*;
 
@@ -10,23 +11,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Pet[] pets = new Pet[2];
+        Toy aToy = new Toy("ball", "purple and white");
 
-        pets[0] = new Cat();
-        pets[1] = new Dog();
+        Cat tweety = new Cat(aToy);
+        System.out.println(tweety.getToy().getColor());
+
+        List<Toy> toyList = new ArrayList<>();
+        toyList.add(new Toy("ball", "black and white"));
+        toyList.add(new Toy("chewing bone", "brown"));
+        toyList.add(new Toy("GIJO", "green"));
+        toyList.add(new Toy("stick", "blue"));
+
+        Dog fluffy = new Dog(toyList);
 
 
-        for(Object i : pets){
-
-            if (i instanceof Cat) {
-                ((Cat)i).move();
-            }
-            if (i instanceof Dog) {
-                ((Dog)i).move();
-            }
-
+        for ( Toy i : fluffy.getToys() ) {
+                System.out.println(i.getName() + " - " + i.getColor());
         }
-
 
     }
 
