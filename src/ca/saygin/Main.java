07f1c39a5867d414c8e8a47f8b5aac9732d12a38
sayginguven <sayginguven@yahoo.com>
2019-10-23 11:53:11 -0700

    testing classroom class

--- src/ca/saygin/Main.java
+++ src/ca/saygin/Main.java
@@ -1,6 +1,9 @@
 package ca.saygin;
 
 import ca.saygin.Classes.Cat;
+import ca.saygin.Classes.Classroom.ClassRoom;
+import ca.saygin.Classes.Classroom.Computer;
+import ca.saygin.Classes.Classroom.Monitor;
 import ca.saygin.Classes.Dog;
 import ca.saygin.Classes.Pet;
 import ca.saygin.Classes.Toy;
@@ -11,23 +14,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Toy aToy = new Toy("ball", "purple and white");
-
-        Cat tweety = new Cat(aToy);
-        System.out.println(tweety.getToy().getColor());
-
-        List<Toy> toyList = new ArrayList<>();
-        toyList.add(new Toy("ball", "black and white"));
-        toyList.add(new Toy("chewing bone", "brown"));
-        toyList.add(new Toy("GIJO", "green"));
-        toyList.add(new Toy("stick", "blue"));
-
-        Dog fluffy = new Dog(toyList);
 
 
-        for ( Toy i : fluffy.getToys() ) {
-                System.out.println(i.getName() + " - " + i.getColor());
-        }
+        ClassRoom class3 = new ClassRoom();
+        class3.getProjector().powerOn();
+        class3.getProjector().powerOff();
+        class3.setTemprature(35.0d);
+
+        System.out.println("temperature of classroom3 : " + class3.getTemprature());
+
+        class3.getWindows().get(0).clean();
+
+        List<Computer> compList = new ArrayList<>();
+        compList.add(new Computer(new Monitor(),"i7", "16gb",true,true,false));
+        compList.add(new Computer(new Monitor(),"i3", "8gb",true,true,false));
+        compList.add(new Computer(new Monitor(),"i5", "24gb",true,true,true));
+        class3.setComputers(compList);
+
+        class3.getComputers().get(2).powerOff();
+
 
     }
 
