0a1e7ef81a79a89f536e3c3bfd5747c948a9e9ad
sayginguven <sayginguven@yahoo.com>
2019-11-20 12:49:32 -0800

    product and categories tested

--- src/ca/saygin/Main.java
+++ src/ca/saygin/Main.java
@@ -1,38 +1,42 @@
 package ca.saygin;
 
-import ca.saygin.Classes.Cat;
-import ca.saygin.Classes.Classroom.ClassRoom;
-import ca.saygin.Classes.Classroom.Computer;
-import ca.saygin.Classes.Classroom.Monitor;
-import ca.saygin.Classes.Dog;
-import ca.saygin.Classes.Pet;
-import ca.saygin.Classes.Toy;
 
-import java.util.*;
+import ca.saygin.DB.Model.Category;
+import ca.saygin.DB.Model.Datasource;
+import ca.saygin.DB.Model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Datasource ds = new Datasource();
 
+        if (ds.open()) {
+            System.out.println("connected to db");
+        } else {
+            System.out.println("not connected to db");
+        }
 
-        ClassRoom class3 = new ClassRoom();
-        class3.getProjector().powerOn();
-        class3.getProjector().powerOff();
-        class3.setTemprature(35.0d);
+        List<Product> productList = new ArrayList<>();
+        productList = ds.queryProducts();
 
-        System.out.println("temperature of classroom3 : " + class3.getTemprature());
+        for(Product product : productList){
+            System.out.println(product.toString());
+        }
 
-        class3.getWindows().get(0).clean();
+        List<Category> categoryList = new ArrayList<>();
+        categoryList = ds.queryCategory();
 
-        List<Computer> compList = new ArrayList<>();
-        compList.add(new Computer(new Monitor(),"i7", "16gb",true,true,false));
-        compList.add(new Computer(new Monitor(),"i3", "8gb",true,true,false));
-        compList.add(new Computer(new Monitor(),"i5", "24gb",true,true,true));
-        class3.setComputers(compList);
+        for(Category category : categoryList){
+            System.out.println(category.toString());
+        }
 
-        class3.getComputers().get(2).powerOff();
-
+        ds.queryProductCategory();
 
     }
 
