bea813d0544004732c984fad5ac8241d2af06cd8
sayginguven <sayginguven@yahoo.com>
2019-12-11 11:58:25 -0800

    5 options added to the menu

--- src/ca/saygin/Main.java
+++ src/ca/saygin/Main.java
@@ -9,42 +9,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        Hashtable<String, String> hashtable = new Hashtable<>();
-//
-//        hashtable.put("Aa","jose");
-//        hashtable.put("BB","khashayar");
-//        hashtable.put("ab","ardeshir");
-//        hashtable.put("aa","sina");
-//        hashtable.put("ba","hamed");
-//        hashtable.put("bb","satoshi");
-//        hashtable.put("bA","daniel");
-//
-//        Enumeration<String> enumeration = hashtable.keys();
-//
-//        while(enumeration.hasMoreElements()){
-//            String key = enumeration.nextElement();
-//            System.out.println(key + " -> " + hashtable.get(key));
-//        }
-//
-//        System.out.println("__ __ __ __");
-//
-//        Set<String> set = hashtable.keySet();
-//
-//        for(String i : set){
-//            System.out.println(i + " -> " + hashtable.get(i));
-//        }
-
-
-
-
-
-
-
         databaseTest();
 
     }
 
     public static void databaseTest(){
+        Scanner sn = new Scanner(System.in);
+        int userInput = 0;
         Datasource ds = new Datasource();
 
         if (ds.open()) {
@@ -53,63 +24,84 @@ public class Main {
             System.out.println("not connected to db");
         }
 
-        ds.insertCustomer("saygin guven", "sayginguven@gmail.com", "Vancouver","604 700 43 42");
+        do{
+
+            System.out.println("\u001B[96m");
+            System.out.println("****************");
+            System.out.println("PLEASE SELECT ONE OF THE ITEM");
+            System.out.println("1 - Insert a customer");
+            System.out.println("2 - List all the products");
+            System.out.println("3 - List all categories");
+            System.out.println("4 - Insert to product and category");
+            System.out.println("5 - List all customers");
+            System.out.println("\u001B[0m");
+
+            userInput = sn.nextInt();
+            sn.nextLine();
+            switch (userInput){
+                case 1: //insert customer
+                    break;
+                case 2://list all the products
+                    ds.queryProductCategoryCurrencies();
+                    break;
+                case 3: //list all the categories
+                    List<Category> categoryList = new ArrayList<>();
+                    categoryList = ds.queryCategory();
+
+                    for(Category category : categoryList){
+                        System.out.println(category.toString());
+                    }
+                    break;
+                case 4: //Insert to product category
+                    try {
+                        System.out.println("what is product name?");
+                        String productName = sn.nextLine();
+                        System.out.println("what is product description?");
+                        String productDescription = sn.nextLine();
+                        System.out.println("what is product price?");
+                        double productPrice = sn.nextDouble();
+                        sn.nextLine();
+                        System.out.println("what is the product currency ID  1-CAD 2-USD 3-JPY");
+                        int currencyId = sn.nextInt();
+                        sn.nextLine();
+                        System.out.println("what is category");
+                        String categoryName = sn.nextLine();
+                        System.out.println("what is sub category");
+                        int subCategory = sn.nextInt();
+                        sn.nextLine();
+
+                        ds.insertProductCategory(productName,
+                                productDescription,
+                                productPrice,
+                                currencyId,
+                                categoryName,
+                                subCategory);
+
+                    } catch (SQLException e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 5: // list all the customers
+                    List<Customer> customerList;
+                    customerList = ds.queryCustomers();
+
+                    for(Customer i : customerList){
+                        System.out.println(i.toString());
+                    }
+
+                    break;
+                case 0:
+                    ds.close();
+                    System.out.println("BYE");
+                    break;
+                default:
+                    System.out.println("this option doesn't exist");
 
 
-
-        List<Product> productList = new ArrayList<>();
-        productList = ds.queryProducts();
-
-        for(Product product : productList){
-            System.out.println(product.toString());
-        }
-
-        List<Category> categoryList = new ArrayList<>();
-        categoryList = ds.queryCategory();
-
-        for(Category category : categoryList){
-            System.out.println(category.toString());
-        }
-
-        ds.queryProductCategoryCurrencies();
-
-        ds.queryProductCategory();
-
-        try {
-
-            ds.insertProductCategory("Cookie",
-                    "cooked with chocolate and mapple srup",
-                    2.25,
-                    1,
-                    "Snack",
-                    5);
-
-        } catch (SQLException e){
-            System.out.println(e.getMessage());
-        }
-
-        try {
-
-            ds.insertProductCategory("Bread",
-                    "fresh baked bread",
-                    1.55,
-                    1,
-                    "Food",
-                    5);
-
-        } catch (SQLException e){
-            System.out.println(e.getMessage());
-        }
+            }
 
 
-        List<Customer> customerList;
-        customerList = ds.queryCustomers();
-
-        for(Customer i : customerList){
-            System.out.println(i.toString());
-        }
-
-        ds.close();
+        }while(userInput != 0);
     }
 
 }
